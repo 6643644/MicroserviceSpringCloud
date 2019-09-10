32745a4c3517dd697daf513d5b9933264c0935a7
MilesChien <q543165@gmail.com>
2019-09-10 10:54:05 +0800

    添加測試類別，簡單練習JUnits

---
+++ SpringCloud-Service01-Config/src/test/java/com/test/main/ConfigMainTest.java
@@ -0,0 +1,17 @@
+package com.test.main;
+
+import org.junit.Test;
+
+public class ConfigMainTest {
+
+	@Test
+	public void Case1() {
+		System.out.println("day 1 test case 1");
+	}
+
+	@Test
+	public void Case2() {
+		System.out.println("day 1 test case 2");
+	}
+
+}
