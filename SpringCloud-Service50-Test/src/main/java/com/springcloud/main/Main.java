961d97293f9be7672c77af3a11b9a1cee67de6b5
MilesChien <q543165@gmail.com>
2020-05-11 15:22:53 +0800

    no message

--- SpringCloud-Service50-Test/src/main/java/com/springcloud/main/Main.java
+++ SpringCloud-Service50-Test/src/main/java/com/springcloud/main/Main.java
@@ -1,21 +1,10 @@
 package com.springcloud.main;
 
-import java.math.BigDecimal;
-
-import com.springcloud.testClass.CalculatorTest;
+import com.springcloud.util.Util;
 
 public class Main {
 
     public static void main(String[] args) {
-//	System.out.println("測試~");
-//	CalculatorTest.testPlus();
-//	CalculatorTest.testMinus();
-	
-	String XchgPrice = "60";
-	String MinBonusPt = "1000";
-//	System.out.println(new BigDecimal(XchgPrice).divide(new BigDecimal(MinBonusPt)));
-//	System.out.println(Double.parseDouble(XchgPrice) / Double.parseDouble(MinBonusPt));
-	System.out.println((Integer.parseInt(XchgPrice) * 14000) / Integer.parseInt(MinBonusPt));
 	
 	
     }
---
+++ SpringCloud-Service50-Test/src/main/java/com/springcloud/util/Util.java
@@ -0,0 +1,8 @@
+package com.springcloud.util;
+
+import com.springcloud.main.Main;
+
+public class Util {
+    
+
+}
